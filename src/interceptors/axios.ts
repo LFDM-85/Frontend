8fd6df60dc40e9e98be9edf4e05edacf82aa081d
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-09-08 16:22:12 +0100

    Update axios.ts

--- src/interceptors/axios.ts
+++ src/interceptors/axios.ts
@@ -1,13 +1,14 @@
 import axios from 'axios';
 // Vercel site
-const BASE_URL = 'https://backend-two-gamma.vercel.app/';
+// const BASE_URL = 'https://backend-two-gamma.vercel.app/';
+const BASE_URL = '';
 
-export default axios.create({
-  baseURL: BASE_URL,
-});
+// export default axios.create({
+//   baseURL: BASE_URL,
+// });
 
 export const axiosPrivate = axios.create({
-  baseURL: BASE_URL,
+  // baseURL: BASE_URL,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
