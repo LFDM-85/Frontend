8075c8cf6198e0e07fdd498976eb971e7e84921f
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-09-12 16:03:37 +0100

    Update MyLayout.tsx

--- src/shared/layouts/MyLayout.tsx
+++ src/shared/layouts/MyLayout.tsx
@@ -4,7 +4,7 @@ import useAuth from '../hooks/useAuth';
 
 export const MyLayout: React.FC<any> = (props): ReactElement => {
   const authCtx = useAuth();
-  const userName = authCtx.user.name;
+  const userName = authCtx.user?.name;
 
   return <Typography ml={6} variant="h4" component="h4">
     Welcome back {userName}
