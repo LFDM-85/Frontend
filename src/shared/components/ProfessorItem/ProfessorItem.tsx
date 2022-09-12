e161f50673ac459f0f3c80a617611fb7d64c3ef1
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-09-12 17:26:04 +0100

    Update ProfessorItem.tsx

--- src/shared/components/ProfessorItem/ProfessorItem.tsx
+++ src/shared/components/ProfessorItem/ProfessorItem.tsx
@@ -1,9 +1,14 @@
+import { Icon } from '@mui/material';
 import useAuth from '../../hooks/useAuth';
+import CheckIcon from '@mui/icons-material/Check';
 export const ProfessorItem = () => {
   const authCtx = useAuth();
 
   return (
-    <div>{authCtx.user.name}</div>
+    <>
+      <div>{authCtx.user.name}</div>
+      <CheckIcon />
+    </>
   );
 };
 
