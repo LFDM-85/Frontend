723ef8a3f0e63098981e3eb6f4ab141e9e67a165
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-09-12 12:25:57 +0100

    Update useRefreshToken.tsx

--- src/shared/hooks/useRefreshToken.tsx
+++ src/shared/hooks/useRefreshToken.tsx
@@ -1,21 +1,21 @@
-// import axios from '../../interceptors/axios';
-// import useAuth from './useAuth';
+import axios from '../../interceptors/axios';
+import useAuth from './useAuth';
 
 const useRefreshToken = () => {
-//   const { setUser }: any = useAuth();
+  const { setUser }: any = useAuth();
 
-//   const refresh = async () => {
-//     const response = await axios.put('/token/refresh', {
-//       withCredentials: true,
-//     });
-//     setUser((prev: any) => {
-//       console.log(JSON.stringify(prev));
-//       console.log(response.data.accessToken);
-//       return { ...prev, accessToken: response.data.token };
-//     });
-//     return response.data.token;
-//   };
-//   return refresh;
+  const refresh = async () => {
+    const response = await axios.put('/token/refresh', {
+      withCredentials: true,
+    });
+    setUser((prev: any) => {
+      console.log(JSON.stringify(prev));
+      console.log(response.data.accessToken);
+      return { ...prev, accessToken: response.data.token };
+    });
+    return response.data.token;
+  };
+  return refresh;
 };
 
 export default useRefreshToken();
