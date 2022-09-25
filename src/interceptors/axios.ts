19c534ec610bf491c1234ca88901355b48bbc152
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-09-25 09:29:22 +0100

    Update axios.ts

--- src/interceptors/axios.ts
+++ src/interceptors/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // Vercel site
-const BASE_URL = 'https://backend-two-gamma.vercel.app/';
+const BASE_URL = 'https://backend-two-gamma.vercel.app';
 // const BASE_URL = 'http://localhost:5000';
 
 
